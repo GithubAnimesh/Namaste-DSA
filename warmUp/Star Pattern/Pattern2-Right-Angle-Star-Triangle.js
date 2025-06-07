5850975b7396483dc19115719ac61785f19dc4cc
Animesh Singh <animesh.singh@gmail.com>
2025-06-07 10:02:08 +0530

    warmup section Star Pattern

---
+++ warmUp/Star Pattern/Pattern2-Right-Angle-Star-Triangle.js
@@ -0,0 +1,34 @@
+// Pattern 2 - Right-Angled Star Triangle Pattern
+// Write a program to print a right-angled triangle of stars (*) with n rows.
+
+function Pattern3(n) {
+  for (let i = 1; i <= n; i++) {
+    let num = "";
+    for (let j = 1; j <= i; j++) {
+      num += j;
+    }
+    console.log(num);
+  }
+}
+
+Pattern3(4);
+
+//OR
+
+function Pattern3(n) {
+  for (let i = 0; i < n; i++) {
+    let num = "";
+    for (let j = 0; j <= i; j++) {
+      num += j + 1;
+    }
+    console.log(num);
+  }
+}
+
+Pattern3(4);
+
+//Output
+// 1
+// 12
+// 123
+// 1234
